487b5ca3779c5c277604df6b788737357937c8ba
hamzarakrouki <hamzarakrouki@gmail.com>
2020-04-09 16:08:52 +0200

    first commit back-end

---
+++ restwebservices/src/test/java/com/projectfullstack/rest/webservices/restwebservices/RestwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectfullstack.rest.webservices.restwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
